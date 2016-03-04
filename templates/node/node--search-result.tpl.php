1f35bf24e518d08d7854c31ac7116dfbc5211fc3
Janette Day <janette@nucivic.com>
2016-03-04 06:23:05 +0000

    Updating search-result.tpl to add topics

--- templates/node/node--search-result.tpl.php
+++ templates/node/node--search-result.tpl.php
@@ -12,7 +12,9 @@
   </div>
   <div class="col-md-10 col-lg-11 col-xs-10 search-result-description">
     <h2 class="node-title"><a href="/<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+    <?php print render($topics) ?>
     <div class="group-membership"><?php print $group_list ?></div>
+    <?php print render($content['field_topic']); ?>
     <?php if(!empty($body)): ?>
       <div class="node-description"><?php print text_summary($body, 'plain_text', 250) ?></div>
     <?php endif; ?>
