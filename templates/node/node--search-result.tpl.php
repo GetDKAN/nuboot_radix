259ca4a10fb4f5f2be5b69645c474c26a63e780f
Janette Day <janette@nucivic.com>
2016-03-22 17:54:19 +0000

    NuCivic#944 Update search results template for resource content

--- templates/node/node--search-result.tpl.php
+++ templates/node/node--search-result.tpl.php
@@ -10,9 +10,10 @@
     print drupal_render($result_icon)
     ?>
   </div>
-  <div class="col-md-10 col-lg-11 col-xs-10 search-result-description">
+  <div class="col-md-10 col-lg-11 col-xs-10 search-result search-result-<?php print $type ?>">
     <h2 class="node-title"><a href="/<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
     <div class="group-membership"><?php print $group_list ?></div>
+    <ul class="dataset-list"><?php print $dataset_list ?></ul>
     <?php if(!empty($body)): ?>
       <div class="node-description"><?php print text_summary($body, 'plain_text', 250) ?></div>
     <?php endif; ?>
